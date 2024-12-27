9ccb920f9072e597f6f31b14fa7e9aa9dbddfbb8
hasibprangon <hasibulhossainp09@gmail.com>
2024-12-27 18:02:28 +0600

    auth interceptor using axios

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useEffect } from 'react';
+import useAuth from './useAuth';
+import { useNavigate } from 'react-router-dom';
 
 const axiosInstance = axios.create({
     baseURL: 'http://localhost:5000',
@@ -7,6 +9,31 @@ const axiosInstance = axios.create({
 })
 
 const useAxiosSecure = () => {
+    const { signOutUser } = useAuth();
+    const navigate = useNavigate();
+
+
+    useEffect(() => {
+        axiosInstance.interceptors.response.use(response => {
+            return response;
+        },
+            error => {
+                console.log('Error caught in interceptor', error);
+                if (error.status === 401 || error.status === 403) {
+                    // console.log('need to logout the user');
+                    signOutUser()
+                    .then(() => {
+                        console.log('Logged Out user');
+                        navigate('/signIn')
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    })
+                }
+                return Promise.reject(error)
+            }
+        )
+    }, [])
     return axiosInstance
 };
 
